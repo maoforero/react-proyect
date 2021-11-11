eaeeaa50672d08af69cec9b64f4dba10395a835f
maoforero <maforeror@gmail.com>
2021-11-11 10:38:36 -0500

    inicio de entrega #2 - componente NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar.js';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
