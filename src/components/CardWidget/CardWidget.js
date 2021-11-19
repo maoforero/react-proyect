b20decc9fa1abf94c10ec82f860312ec21560832
maoforero <maforeror@gmail.com>
2021-11-18 22:49:17 -0500

    delete reportWebVitals setupTest logo AppTest

--- src/components/CardWidget/CardWidget.js
+++ src/components/CardWidget/CardWidget.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "../CardWidget/CardWidget.css"
+import icon_logo from "https://cdn-icons.flaticon.com/png/512/2211/premium/2211008.png?token=exp=1637221998~hmac=9448ca4b8ddb2afddadba1fc4cdb7478"
 
 const CardWidget = () => {
     return (
         <>
-            <img className="icon__carrito" src="https://cdn-icons.flaticon.com/png/512/2211/premium/2211008.png?token=exp=1637221998~hmac=9448ca4b8ddb2afddadba1fc4cdb7478"></img>
+            <img className="icon__carrito" src={icon_logo}></img>
         </>
     )
 }
