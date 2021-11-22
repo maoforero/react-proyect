a24d5fe32859be029cec858550f5aea3d468a542
maoforero <maforeror@gmail.com>
2021-11-21 23:46:57 -0500

    estructura base de ItemCount

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const ItemCount = () => {
+const ItemCount = ({title}) => {
+    
+    let contador = 0;
+    
     return (
         <>
+            <div>
+                <h1>{title}</h1>
+            </div>
+            <div>
+                <p></p>
+            </div>
+            <div>
+                <button className="count_button">+</button>
+                <button className="count_button">-</button>
+            </div>
             
+
         </>
     )
 }
