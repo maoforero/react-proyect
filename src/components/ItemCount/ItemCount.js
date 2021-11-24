26b44cddb82c2000e7adf78c54e128c10f88bdec
maoforero <maforeror@gmail.com>
2021-11-23 23:48:09 -0500

    Se agregaron dos funciones en el componente ItemCount. addClick y removeClick

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ItemCount = ({title}) => {
-    
-    let contador = 0;
-    
+
+    const [click, setClick] = useState(0);
+
+    //Funcion incremento de clicks
+    const addClick = () =>{
+        if(click > 9){
+            alert(`cantidad Limitada`);
+        }else{
+            setClick(click + 1);
+        };
+    };
+
+    //Funcion decremento de clicks
+    const removeClick = () => {
+        if(click > 0){
+            setClick (click - 1);
+        }else{
+            alert(`Seleccione un producto`)
+        }
+    }
+
     return (
+
         <>
             <div>
                 <h1>{title}</h1>
