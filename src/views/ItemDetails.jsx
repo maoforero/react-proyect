a991069472523815bd539893d52c429dfeabb46b
maoforero <maforeror@gmail.com>
2021-12-17 23:58:29 -0500

    Setting itemDetails por ID

--- src/views/ItemDetails.jsx
+++ src/views/ItemDetails.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import listaElementos from '../list.json'
 
 const ItemDetails = () => {
+    const [element, setElement] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setElement(listaElementos)
+        }, 1500);
+
+    }, [])
+
     return (
-        <div>
-            
+        <div className='container__mainItemDetails'>
+            <h1>Detalles del producto</h1>
         </div>
     )
 }
