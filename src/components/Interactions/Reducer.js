625fc8c910b3a0aba49010c490287a66e7fe3b55
maoforero <maforeror@gmail.com>
2022-01-09 23:16:53 -0500

    styles error page

--- src/components/Interactions/Reducer.js
+++
@@ -1,4 +0,0 @@
-export const carState = {
-    products:[],
-    car:[]
-}
--- src/views/Error/Error.js
+++ src/views/Error/Error.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import imgError from '../../assets/404-error.svg';
+import './Error.css';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Error</h1>
+        <div className='container__errorView'>
+            <div className='errorView--title'>
+                <h1 className='errorTitle'>Error :(</h1>
+            </div>
+            <div className='errorView--text'>
+                <p className='errorText'>Lo lamentamos, la ruta seleccionada no se encuentra disponible...</p>
+            </div>
+            <div className='errorView--img'>
+                <img src={imgError} alt="imagen de error" className='errorImg'/>
+            </div>
         </div>
     )
 }
